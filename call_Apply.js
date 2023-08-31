9027d25da06d9a8f3aaa2b3674eb73f529c226e0
MdShahariarHafiz <shahariarwebdev@gmail.com>
2023-08-31 22:20:22 +0600

    apply call concept added

--- call_Apply.js
+++ call_Apply.js
@@ -1,15 +1,45 @@
 const normalPerson = {
-    firstName: "sogir",
-    lastName: "Uddin",
-    selary: 17000,
+    firstName: "soggier",
+    lastName: "Undine",
+    salary: 17000,
     getFullName: function(){
         console.log(`${this.firstName} ${this.lastName}`);
     },
     cBill: function(amount) {
-        this.selary =  this.selary - amount;
-        return this.selary;
+        this.salary =  this.salary - amount;
+        return this.salary;
     }, 
 }
 
-normalPerson.cBill(5000);
-console.log(normalPerson);
+const vipParson = {
+    firstName: "Coudhury",
+    lastName: "Saheb",
+    salary: 50000,
+    getFullName: function(){
+        console.log(`${this.firstName} ${this.lastName}`);
+    },
+}
+
+const minister = {
+    firstName: "Imran",
+    lastName: "Khan",
+    salary: 80000,
+    getFullName: function(){
+        console.log(`${this.firstName} ${this.lastName}`);
+    },
+}
+
+// const vipCBill = normalPerson.cBill.bind(vipParson);
+// vipCBill(5000);
+// vipCBill(7000)
+// console.log(vipParson);
+
+// const ministerBill = normalPerson.cBill.bind(minister);
+// ministerBill(50000);
+// console.log(minister);
+
+normalPerson.cBill.call(vipParson, 5000)
+console.log(vipParson.salary);
+
+normalPerson.cBill.apply(minister, [70000]);
+console.log(minister.salary);
---
+++ class.js
@@ -0,0 +1,15 @@
+class Parson {
+    constructor(name, age, salary){
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+}
+
+const parson1 = new Parson("Hero", 27, 80000);
+console.log(parson1);
+
+const parson2 = new Parson("Alom", 56, 1200);
+console.log(parson2);
+const parson3 = new Parson("Kalom", 37, 800000);
+console.log(parson3);
